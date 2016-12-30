f112a9f4e97a999c4ed045f3b8bd04b9e57e61b6
Jack Zhou <jack@zzh.ca>
2016-12-29 21:35:15 -0500

    Add HHKB-like Satan GH60 mappings

--- keyboards/satan/keymaps/default/keymap.c
+++ keyboards/satan/keymaps/default/keymap.c
@@ -9,58 +9,71 @@
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _BL 0
-#define _FL 1
+#define _FL1 1
+#define _FL2 2
 
 #define _______ KC_TRNS
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: (Base Layer) Default Layer
    * ,-----------------------------------------------------------.
-   * |Esc~| 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |
+   * |Esc~| 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =| \ | ` |
    * |-----------------------------------------------------------|
-   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |
+   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|Bksp |
    * |-----------------------------------------------------------|
-   * |CAPS   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
+   * |Ctrl   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
    * |-----------------------------------------------------------|
-   * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |
+   * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |FN1|
    * |-----------------------------------------------------------|
-   * |Ctrl|Gui |Alt |      Space            |Alt |Gui |FN  |Ctrl |
+   * |FN2 |Alt |Gui |      Space            |Gui |Alt |FN2 |Ctrl |
    * `-----------------------------------------------------------'
    */
-[_BL] = KEYMAP_ANSI(
-  F(0),    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC, \
-  KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS, \
-  KC_CAPS, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         KC_ENT,  \
-  KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,         KC_RSFT, \
-  KC_LCTL, KC_LGUI,KC_LALT,                KC_SPC,                                 KC_RALT,KC_RGUI, MO(_FL),KC_RCTL),
+[_BL] = KEYMAP_HHKB(
+  F(0),    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_GRV, KC_BSLS, \
+  KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSPC, \
+  KC_LCTL, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         KC_ENT,  \
+  KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSFT,MO(_FL1), \
+  MO(_FL2),KC_LGUI,KC_LALT,                KC_SPC,                                 KC_RGUI,KC_RALT,MO(_FL2),KC_RCTL),
 
-  /* Keymap _FL: Function Layer
+  /* Keymap _FL1: (Function Layer 1) Default Function Layer
    * ,-----------------------------------------------------------.
-   * |   |   |   |   |   |   |   |   |   |   |   |   |   |  RESET|
+   * | ` |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Ins|Del|
    * |-----------------------------------------------------------|
-   * |     |   |   |   |   |   |   |   |   |   |   |BL-|BL+|BL   |
+   * |Caps |Prv|Ply|Nxt|Stp|   |   |   |Psc|SLk|Pus|Up |   |     |
    * |-----------------------------------------------------------|
-   * |      |   |   |   |   |   |   |       |   |   |   |        |
+   * |       |V- |V+ |Vm |   |   | * | / |Hme|PgU|Lft|Rgt|       |
    * |-----------------------------------------------------------|
-   * |        | F1|F2 | F3|F4 | F5| F6| F7| F8|   |   |          |
+   * |        |F13|F14|   |   |   | + | - |End|PgD|Dwn|      |   |
    * |-----------------------------------------------------------|
-   * |    |    |    |                        |    |    |    |    |
+   * |    |    |    |                       |    |    |    |     |
    * `-----------------------------------------------------------'
    */
-[_FL] = KEYMAP_ANSI(
-  #ifdef RGBLIGHT_ENABLE
-  KC_GRV, _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,RESET,  \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, BL_DEC,BL_INC, BL_TOGG, \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        _______, \
-  _______,RGB_TOG,RGB_MOD,RGB_HUI,RGB_HUD,RGB_SAI,RGB_SAD,RGB_VAI,RGB_VAD,_______,_______,_______, \
-  _______,_______,_______,                 _______,                       _______,_______,_______, _______),
-  #else
-  KC_GRV, _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,RESET,  \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, BL_DEC, BL_INC,BL_TOGG, \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, \
+[_FL1] = KEYMAP_HHKB(
+  KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_INS, KC_DEL,  \
+  KC_CAPS,KC_MPRV,KC_MPLY,KC_MNXT,KC_MSTP,_______,_______,_______,KC_PSCR,KC_SLCK,KC_PAUS,KC_UP  ,_______,_______, \
+  _______,KC_VOLD,KC_VOLU,KC_MUTE,_______,_______,KC_PAST,KC_PSLS,KC_HOME,KC_PGUP,KC_LEFT,KC_RGHT,_______, \
+  _______,KC_F13 ,KC_F14 ,_______,_______,_______,KC_PPLS,KC_PMNS,KC_END ,KC_PGDN,KC_DOWN,_______,_______, \
   _______,_______,_______,                _______,                        _______,_______,_______,_______),
-  #endif
+
+  /* Keymap _FL2: (Function Layer 2) Basic Vim mappings + Numpad
+   * ,-----------------------------------------------------------.
+   * |   |   |   |   | / |   |   |   |   |   |   |   |   |   |   |
+   * |-----------------------------------------------------------|
+   * |     |Kp7|Kp8|Kp9| * |   |   |   |   |   |   |   |   |     |
+   * |-----------------------------------------------------------|
+   * |KpEnter|Kp4|Kp5|Kp6| - |   |Lft|Dwn|Up |Rgt|   |   |       |
+   * |-----------------------------------------------------------|
+   * |        |Kp1|Kp2|Kp3| + |   |   |   |   |   |   |      |   |
+   * |-----------------------------------------------------------|
+   * |    |    |Kp0 |                       |    |    |    |     |
+   * `-----------------------------------------------------------'
+   */
+[_FL2] = KEYMAP_HHKB(
+  _______,_______,_______,_______,KC_PSLS,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,  \
+  _______,KC_P7  ,KC_P8  ,KC_P9  ,KC_PAST,_______,_______,_______,_______,_______,_______,_______,_______,_______, \
+  KC_PENT,KC_P4  ,KC_P5  ,KC_P6  ,KC_PMNS,_______,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,_______,_______,_______, \
+  _______,KC_P1  ,KC_P2  ,KC_P3  ,KC_PPLS,_______,_______,_______,_______,_______,_______,_______,_______, \
+  _______,_______,KC_P0  ,                _______,                        _______,_______,_______,_______),
 };
 
 enum function_id {
---
+++ make_satan.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd keyboards/satan/keymaps/default
+make
