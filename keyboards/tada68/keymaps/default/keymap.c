0978855484bd0b891c2ebff127930b3c30d865e5
Jack Zhou <jack@zzh.ca>
2017-06-12 13:27:43 -0700

    Try a different Saber68 layout

--- keyboards/tada68/keymaps/default/keymap.c
+++ keyboards/tada68/keymaps/default/keymap.c
@@ -5,11 +5,10 @@
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _BL  0
-#define _AL  1
-#define _TL  2
-#define _MB  3
-#define _FL1 4
-#define _FL2 5
+#define _TL  1
+#define _MB  2
+#define _FL  3
+#define _VL  4
 
 #define _______ KC_TRNS
 
@@ -53,31 +52,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `---------------------------------------------------------------'
    */
 [_BL] = KEYMAP_ANSI(
-  F(0),    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSLS, KC_GRV, \
-  KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSPC, KC_INS, \
-  KC_LCTL, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,     KC_ENT,      KC_DEL, \
-  KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,     KC_RSFT,    MO(_FL1),KC_UP,  \
-  MO(_FL2),KC_LALT,KC_LGUI,                KC_SPC,                         KC_RGUI,KC_RALT,KC_RCTRL,KC_LEFT,KC_RGHT, KC_DOWN),
-
-  /* Keymap _AL: Arrows Layer
-   * ,---------------------------------------------------------------.
-   * |   |   |   |   |   |   |   |   |   |   |   |   |   |       |   |
-   * |---------------------------------------------------------------|
-   * |     |   |   |   |   |   |   |   |   |   |   |   |   |     |   |
-   * |---------------------------------------------------------------|
-   * |      |   |   |   |   |   |   |   |   |   |   |   |        |   |
-   * |---------------------------------------------------------------|
-   * |        |   |   |   |   |   |   |   |   |   |   |      |Up |Fn1|
-   * |---------------------------------------------------------------|
-   * |    |    |    |                       |   |   |    |   |   |   |
-   * `---------------------------------------------------------------'
-   */
-[_AL] = KEYMAP_ANSI(
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,  \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,  \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,    _______,    _______,  \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,    _______,    KC_UP,  MO(_FL1), \
-  _______,_______,_______,                   _______,                     _______,_______,_______,_______,KC_DOWN,KC_RGHT),
+  F(0),   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSLS,KC_GRV,  \
+  KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSPC,KC_INS,  \
+  KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,     KC_ENT,     KC_DEL,  \
+  KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,     KC_RSFT,    KC_UP,  MO(_FL), \
+  MO(_VL),KC_LALT,KC_LGUI,                KC_SPC,                         KC_RGUI,KC_RALT,KC_RCTRL,KC_LEFT,KC_DOWN,KC_RGHT),
 
   /* Keymap _TL: Traditional Layer
    * ,---------------------------------------------------------------.
@@ -123,23 +102,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------.
    * | ` |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12| Del   |BL  |
    * |----------------------------------------------------------------|
-   * |Caps |Prv|Ply|Nxt|Stp|   |   |   |Psc|SLk|Pus|Up |   |     |BL+ |
+   * |Caps |Prv|Ply|Nxt|Stp|   |   |   |Psc|SLk|Pus|   |Up |     |BL+ |
    * |----------------------------------------------------------------|
-   * |      |V- |V+ |Vm |   |   | * | / |Hme|PgU|Lft|Rgt|        |BL- |
+   * |      |V- |V+ |Vm |   |   | * | / |   |Hme|PgU|Dwn|        |BL- |
    * |----------------------------------------------------------------|
-   * |        |F14|F15|AL |MB |TL | + | - |End|PgD|Dwn|      |   |    |
+   * |        |F14|F15|AL |MB |TL | + | - |   |End|PgD|      |   |    |
    * |----------------------------------------------------------------|
    * |    |    |    |                       |   |   |    |   |   |    |
    * `----------------------------------------------------------------'
    */
-[_FL1] = KEYMAP_ANSI(
+[_FL] = KEYMAP_ANSI(
   KC_GRV, KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_DEL ,BL_TOGG, \
-  KC_CAPS,KC_MPRV,KC_MPLY,KC_MNXT,KC_MSTP,_______,_______,_______,KC_PSCR,KC_SLCK,KC_PAUS,KC_UP  ,_______,_______,BL_INC,  \
-  _______,KC_VOLD,KC_VOLU,KC_MUTE,_______,_______,KC_PAST,KC_PSLS,KC_HOME,KC_PGUP,KC_LEFT,KC_RGHT,    _______,    BL_DEC,  \
-  _______,KC_F14 ,KC_F15 ,TG(_AL),TG(_MB),TG(_TL),KC_PPLS,KC_PMNS,KC_END ,KC_PGDN,KC_DOWN,    _______,    _______,_______, \
+  KC_CAPS,KC_MPRV,KC_MPLY,KC_MNXT,KC_MSTP,_______,_______,_______,KC_PSCR,KC_SLCK,KC_PAUS,_______,KC_UP  ,_______,BL_INC,  \
+  _______,KC_VOLD,KC_VOLU,KC_MUTE,_______,_______,KC_PAST,KC_PSLS,_______,KC_HOME,KC_PGUP,KC_DOWN,    _______,    BL_DEC,  \
+  _______,KC_F14 ,KC_F15 ,_______,TG(_MB),TG(_TL),KC_PPLS,KC_PMNS,_______,KC_END ,KC_PGDN,    _______,    _______,_______, \
   _______,_______,_______,                   _______,                     _______,_______,_______,_______,_______,_______),
 
-  /* Keymap _FL2: Function Layer 2
+  /* Keymap _VL: Vim/Numpad Layer
    * ,---------------------------------------------------------------.
    * |   |   |   |   | / |   |   |   |   |   |   |   |   |       |   |
    * |---------------------------------------------------------------|
@@ -147,17 +126,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |---------------------------------------------------------------|
    * |KpEntr|Kp4|Kp5|Kp6| - |   |Lft|Dwn|Up |Rgt|   |   |        |   |
    * |---------------------------------------------------------------|
-   * |        |Kp1|Kp2|Kp3| + |   |   |   |   |   |   |      |   |   |
+   * | Kp=    |Kp1|Kp2|Kp3| + |   |   |   |   |   |   |      |   |   |
    * |---------------------------------------------------------------|
-   * |    |    |Kp0 |                       |   |   |    |   |   |   |
+   * |    |    |Kp0 | Kp.                   |   |   |    |   |   |   |
    * `---------------------------------------------------------------'
    */
-[_FL2] = KEYMAP_ANSI(
+[_VL] = KEYMAP_ANSI(
   _______,_______,_______,_______,KC_PSLS,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, \
   KC_NLCK,KC_P7  ,KC_P8  ,KC_P9  ,KC_PAST,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, \
   KC_PENT,KC_P4  ,KC_P5  ,KC_P6  ,KC_PMNS,_______,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,_______,_______,    _______,    _______, \
-  _______,KC_P1  ,KC_P2  ,KC_P3  ,KC_PPLS,_______,_______,_______,_______,_______,_______,    _______,    _______,_______, \
-  _______,_______,KC_P0  ,                _______,                        _______,_______,_______,_______,_______,_______),
+  KC_PEQL,KC_P1  ,KC_P2  ,KC_P3  ,KC_PPLS,_______,_______,_______,_______,_______,_______,    _______,    _______,_______, \
+  _______,_______,KC_P0  ,                    KC_PDOT,                    _______,_______,_______,_______,_______,_______),
 };
 
 enum function_id {
