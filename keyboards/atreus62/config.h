a9662b6ed4eec036d1373e359887f6eba1e2340a
Jack Zhou <jack@zzh.ca>
2017-08-06 19:31:52 -0700

    Tweak Atreus62 mouse settings

--- keyboards/atreus62/config.h
+++ keyboards/atreus62/config.h
@@ -82,8 +82,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define MOUSEKEY_INTERVAL    20
 #define MOUSEKEY_DELAY       0
-#define MOUSEKEY_TIME_TO_MAX 20
+#define MOUSEKEY_TIME_TO_MAX 15
 #define MOUSEKEY_MAX_SPEED   4
-#define MOUSEKEY_WHEEL_DELAY 1
+#define MOUSEKEY_WHEEL_DELAY 10
 
 #endif
