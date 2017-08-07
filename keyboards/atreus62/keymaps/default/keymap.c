a932353d8dd5151cf1c0743c70d6f9f17dfc191a
Jack Zhou <jack@zzh.ca>
2017-08-06 18:16:32 -0700

    Deduplicate square braces on Atreus layout

--- keyboards/atreus62/keymaps/default/keymap.c
+++ keyboards/atreus62/keymaps/default/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
     MO(_NL), KC_GRV,  KC_INS,  KC_LALT, KC_LGUI, KC_BSPC, LT(_ML,KC_ESC),
-                                                                   KC_ENT,  KC_SPC,  KC_LBRC, KC_RBRC, KC_LBRC, KC_RBRC, MO(_FL)),
+                                                                   KC_ENT,  KC_SPC,  KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, MO(_FL)),
 
   // Keymap _FL: (Function Layer) Default Function Layer
   [_FL] = KEYMAP(
